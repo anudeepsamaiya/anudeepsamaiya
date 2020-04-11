fd30b6bf9428fa4781cc353c9c053bfc6121b8ac
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2020-04-11 20:17:55 +0530

    configure travis

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+cd site/public
+
+if [ -n "$GITHUB_AUTH_SECRET" ]
+then
+    touch ~/.git-credentials
+    chmod 0600 ~/.git-credentials
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+
+    git config credential.helper store
+    git config user.email "anudeepsamaiya-blog-bot@users.noreply.github.com"
+    git config user.name "anudeepsamaiya-blog-bot"
+fi
+
+git add .
+git commit -m "Rebuild site"
+git push --force origin HEAD:master
