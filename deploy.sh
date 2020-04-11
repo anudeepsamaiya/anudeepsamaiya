4f7ee7dc111f93c42f276f8605103a769b9d3b46
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2020-04-11 22:46:33 +0530

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 cd site/public
 
-if [ -n "$GITHUB_AUTH_SECRET" ]
+if [ -n "$AUTH_SECRET" ]
 then
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
-    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+    echo $AUTH_SECRET > ~/.git-credentials
 
     git config credential.helper store
-    git config user.email "anudeepsamaiya-blog-bot@users.noreply.github.com"
+    git config user.email "63507695+anudeepsamaiya-blog-bot@users.noreply.github.com"
     git config user.name "anudeepsamaiya-blog-bot"
 fi
 
